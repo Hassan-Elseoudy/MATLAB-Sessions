2a84bafd3f8975f684dbc81fcf3868f5ceac2517
[Hassan El-seoudy] <[hassanzamalek97@gmail.com]>
2018-05-17 00:55:38 +0200

    Project is added.

---
+++ Project/New folder/Q1.m
@@ -0,0 +1,67 @@
+﻿% Q.a) 1.
+n1 = [-12:12];
+x1 = [-2,-1,0,1,2];
+xQ1 = [x1,x1,x1,x1,x1];
+stem(n1,xQ1,'--');
+
+% Q.a) 2.
+n2 = [-22:43];
+x2 = exp(0.1*[0:21]).*(stepseq(0,0,21)-stepseq(20,0,21)); %ends @ 21
+xQ2 = [x2,x2,x2];
+stem(n2,xQ2,'--');
+
+% Q.a) 3.
+n3 = [-12:35];
+x3 = sin(0.1*pi*[0:11]).*(stepseq(0,0,11)-stepseq(10,0,11)); %ends @ 11
+xQ3 = [x3,x3,x3,x3];
+stem(n3,xQ3,'--');
+
+% Q.a) 4.
+n4 = [0:24];
+x4a1 = [1 2 3];
+x4a = [x4a1 x4a1 x4a1 x4a1 x4a1 x4a1 x4a1 x4a1 x4a1];
+x4b1 = [1 2 3 4];
+x4b = [x4b1 x4b1 x4b1 x4b1 x4b1 x4b1 x4b1];
+xQ4 = x4a(1:25) + x4b(1:25);
+stem(n4,xQ4,'--');
+
+%-----------------------------------------------------------------------
+n= [-3:3];
+x = [2,4,-3,1,-5,4,7];
+
+%Q.b)1
+[x11,n11] = sigshift(x,n,3);
+[x12,n12] = sigshift(x,n,-4);
+[x13,n13] = sigadd(2*x11,n11,3*x12,n12);
+[xQ1b,n1b] = sigadd(x13,n13,-x,n);
+stem(n1b,xQ1b,'-.');
+
+%Q.b)2
+[x21,n21] = sigshift(x,n,-4);
+[x22,n22] = sigshift(x,n,-5);
+[x23,n23] = sigadd(4*x21,n21,5*x22,n22);
+[xQ2b,n2b] = sigadd(x23,n23,2*x,n);
+stem(n2b,xQ2b,'-.');
+
+%Q.b)3
+[x31,n31] = sigshift(x,n,-3);
+[x32,n32] = sigshift(x,n,+2);
+[x33,n33] = sigshift(x,n,-1);
+[x34,n34] = sigfold(x33,n33);
+[x35,n35] = sigshift(x,n,-1);
+[x31b,n31b] = sigmult(x31,n31,x32,n32);
+[x32b,n32b] = sigmult(x34,n34,x35,n35);
+[xQ3b,n3b] =sigadd(x31b,n31b,x32b,n32b);
+stem(n3b,xQ3b,'-.');
+
+%Q.b)4 
+n4 = [-10:10]; 
+x41 = 2*exp(0.5*n4); 
+x412 = cos(0.1*pi*n4);
+[x42,n42] = sigmult(x41,n4,x,n);
+[x43,n43] = sigshift(x,n,-2);
+[x44,n44] = sigmult(x412,n42,x43,n43);
+[xQ4b,n4b] = sigadd(x42,n42,x44,n44);
+stem(n4b,xQ4b,'-.');
+
+%-----------------------------------------------------------------------
---
+++ Project/New folder/Q4.m
@@ -0,0 +1,58 @@
+﻿6% Q.4) a
+x1 = stepseq(0,0,10) - stepseq(6,0,10);
+X1 = fft(x1);
+stem(abs(X1));
+stem(angle(X1));
+
+% Q.4) b
+n2 = [0:10];
+x21 = 2 .^ n2;
+[x22,n22] = stepseq(0,0,10);
+[x22,n22] = sigfold(x22,n22);
+[x2,n2] = sigmult(x21,n2,x22,n22);
+X2 = fft(x2);
+stem(abs(X2));
+stem(angle(X2));
+
+% Q.4) c
+n31 = [0:10];
+x31 = 0.25 .^ n31;
+[x32,n32] = stepseq(0,0,10);
+[x3,n3] = sigmult(x31,n31,x32,n32);
+X3 = fft(x3);
+stem(abs(X3));
+stem(angle(X3));
+
+% Q.4) d
+n4 = [0:10];
+[x41,n41] = stepseq(0,0,10);
+x42 = 0.25 .^ n4;
+x43 = sin(2*pi*0.25*n4);
+[x4,n4] = sigmult(x42,n4,x43,n4);
+[x4,n4] = sigmult(x4,n4,x41,n41);
+X4 = fft(x4);
+stem(abs(X4));
+stem(angle(X4));
+
+% Q.4) e
+n5 = [0:10];
+x52 = 0.5 .^ n5;
+x53 = sin(2*pi*0.25*n5);
+[x5,n5] = sigmult(x52,n5,x53,n5);
+X5 = fft(x5);
+stem(abs(X5))
+stem(angle(X5))
+
+% Q.4) f
+n6 = [-4:4];
+x6 = 2 - 0.5*n6;
+X6 = fft(x6);
+stem(abs(X6));
+stem(angle(X6));
+
+% Q.4) g
+n7 = [-2:2];
+x7 = [-2 -1 0 1 2];
+X7 = fft(x7);
+stem(abs(X7));
+stem(angle(X7));
